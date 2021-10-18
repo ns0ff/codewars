41e10dde7ba22974ee13f8660e28bb9929a47260
Nick <74413828+ns0ff@users.noreply.github.com>
2021-10-18 17:31:23 +0300

    Reverse words initial commit
    Codewars reverse words kata solution, JS.

---
+++ reverse-words.js
@@ -0,0 +1,9 @@
+function reverseWords(str) {
+    return str.split('').reverse().join('').split(' ').reverse().join(' ');
+}
+
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.')); // 'ehT kciuq nworb xof spmuj revo eht yzal .god'
+console.log(reverseWords('apple')); // 'elppa'
+console.log(reverseWords('a b c d')); // 'a b c d'
+console.log(reverseWords('double  spaced  words')); // 'elbuod  decaps  sdrow'
