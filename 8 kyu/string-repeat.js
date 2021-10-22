7d9aa4dfd8b43bab5d1d044669191404baca6260
Nick <74413828+ns0ff@users.noreply.github.com>
2021-10-22 10:36:53 +0300

    String repeat initial commit
    Codewars String repeat kata solution, JS. 8 kyu.

---
+++ 8 kyu/string-repeat.js
@@ -0,0 +1,15 @@
+function repeatStr (n, s) {
+    let newStr = '';
+    
+    for (i = 0; i < n; i++){
+      newStr += s;
+    }
+    
+    return newStr;
+}
+
+// Tests
+console.log(repeatStr(3, '*')); // return '***'
+console.log(repeatStr(5, '#')); // return '#####'
+console.log(repeatStr(2, 'ha ')); // return 'ha ha'
+console.log(repeatStr(3, 'hello ')); // return 'hello hello hello'
