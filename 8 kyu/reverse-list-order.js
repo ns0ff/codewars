df62bf2800bde1146c70f0e9f4061252d0b2d66d
Nick <74413828+ns0ff@users.noreply.github.com>
2021-10-24 08:00:02 +0300

    Reverse List Order initial commit
    Codewars Reverse List Order kata sollution, JS. 8 kyu

---
+++ 8 kyu/reverse-list-order.js
@@ -0,0 +1,7 @@
+function reverseList(list) {
+    return list.reverse();
+}
+
+// Tests
+console.log(reverseList([1,2,3,4])); // return [4,3,2,1]
+console.log(reverseList([3,1,5,4])); // return [4,5,1,3]
