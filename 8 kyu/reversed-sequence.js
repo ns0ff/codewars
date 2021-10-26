ca069fabb406f09a1d5b6103501920ca5a2a1753
Nick <74413828+ns0ff@users.noreply.github.com>
2021-10-26 07:40:50 +0300

    Code refactoring
    Single line code refactoring

--- 8 kyu/reversed-sequence.js
+++ 8 kyu/reversed-sequence.js
@@ -1,14 +1,19 @@
 // https://www.codewars.com/kata/5a00e05cc374cb34d100000d
 
 const reverseSeq = n => {
-    let arr = [];
+  // Single line
+  return Array(n).fill(0).map((e, i) => n - i );
 
-    for ( i = n; i >= 1; i--) {
-      arr.push(i);
-    }
-    
-    return arr;
+  // Previous solution:
+    // let arr = [];
+
+    // for ( i = n; i >= 1; i--) {
+    //   arr.push(i);
+    // }
+
+    // return arr;
+
 };
 
 // Test
-console.log(reverseSeq(5)); // return [5, 4, 3, 2, 1]
+console.log(reverseSeq(5)); // expected output: [5, 4, 3, 2, 1]
