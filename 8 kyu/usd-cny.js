c06d64f93c72cca4a188e0304170ee5af12d8e34
Nick <74413828+ns0ff@users.noreply.github.com>
2021-10-27 08:04:15 +0300

    USD => CNY initial commit
    Codewars USD => CNY kata solution, JS. 8 kyu.

---
+++ 8 kyu/usd-cny.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5977618080ef220766000022
+
+function usdcny(usd) {
+    let cny = parseFloat(usd).toFixed(2) * 6.75;
+    return  cny.toFixed(2) + ' Chinese Yuan';
+}
+
+// Tests
+console.log(usdcny(15)); // expected output: '101.25 Chinese Yuan'
+console.log(usdcny(465)); // expected output: '3138.75 Chinese Yuan'
