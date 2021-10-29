8e9cb273e74ef3d58d57e688d84c318f3c2c519c
Nick <74413828+ns0ff@users.noreply.github.com>
2021-10-29 09:34:27 +0300

    other solution added

--- 8 kyu/usd-cny.js
+++ 8 kyu/usd-cny.js
@@ -5,6 +5,8 @@ function usdcny(usd) {
     return  cny.toFixed(2) + ' Chinese Yuan';
 }
 
+ // usdcny = usd => `${(usd * 6.75).toFixed(2)} Chinese Yuan`;
+
 // Tests
 console.log(usdcny(15)); // expected output: '101.25 Chinese Yuan'
 console.log(usdcny(465)); // expected output: '3138.75 Chinese Yuan'
