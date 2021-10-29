3022ce72093ba056825c3b6374591b0b75961cd2
Nick <74413828+ns0ff@users.noreply.github.com>
2021-10-29 09:34:31 +0300

    other solution added

--- 8 kyu/return-negative.js
+++ 8 kyu/return-negative.js
@@ -1,7 +1,10 @@
-// 
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
 
 function makeNegative(num) {
     return num >= 0 ? num - (num * 2) : num; 
+
+    // Other solution:
+    // return num < 0 ? num : -num;
 }
 
 // Tests
