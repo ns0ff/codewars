07aad8e38889b423ccd339fb68ec0f91db73ced8
Nick <74413828+ns0ff@users.noreply.github.com>
2021-12-06 21:49:53 +0300

    FIXME: Replace all dots solution added
    Codewars FIXME: Replace all dots kata solution, JS. 8 kyu.

---
+++ 8 kyu/ replace-all-dots.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/596c6eb85b0f515834000049
+
+const replaceDots = (str) => str.replace(/\./g, '-')
+
+// Output:
+console.log(replaceDots("one.two.three")); // expected output: "one-two-three"
