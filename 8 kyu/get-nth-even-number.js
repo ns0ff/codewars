bf4462d5a4d2a95272e901206dc42dd3c48750ea
Nick <74413828+ns0ff@users.noreply.github.com>
2021-12-19 21:59:51 +0300

    Get Nth Even Number solution added
    Codewars Get Nth Even Number kata solution, JS. 8 kyu.

---
+++ 8 kyu/get-nth-even-number.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed
+
+const nthEven = n => (n - 1) * 2
+
+// Output:
+console.log(nthEven(1))  // expected output: 0
+console.log(nthEven(2))  // expected output: 2
+console.log(nthEven(3))  // expected output: 4
+console.log(nthEven(100))  // expected output: 198
+console.log(nthEven(1298734))  // expected output: 2597466
