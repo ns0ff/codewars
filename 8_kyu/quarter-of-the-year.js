23b603c0b5049959ab5e180985cdcb680b7ae6b2
Nick <74413828+ns0ff@users.noreply.github.com>
2022-08-03 00:54:52 +0300

    Quarter of the year kata solution added

---
+++ 8_kyu/quarter-of-the-year.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+
+const quarterOf = (m) => Math.ceil(m /3)
+
+// Output:
+console.log(quarterOf(3)); // Expected output: 1
+console.log(quarterOf(8)); // Expected output: 3
+console.log(quarterOf(11)); // Expected output: 4
