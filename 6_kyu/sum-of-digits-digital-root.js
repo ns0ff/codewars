9ff1942b49d81b21e2a17d1e08b16f68fa7d39df
Nick <74413828+ns0ff@users.noreply.github.com>
2022-08-13 23:22:40 +0300

    Sum of Digits / Digital Root kata solution added
    JS 6 kyu

---
+++ 6_kyu/sum-of-digits-digital-root.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/541c8630095125aba6000c00
+
+const digitalRoot = x => (x - 1) % 9 + 1
+
+// Output:
+console.log(digitalRoot(16));  // expected output: 7
+console.log(digitalRoot(456));  // expected output: 6
