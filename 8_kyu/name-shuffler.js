0ba92dfb92ea5098a0e2363886648ad123f50603
Nick <74413828+ns0ff@users.noreply.github.com>
2022-08-26 22:21:00 +0300

    name shuffler kata solution added

---
+++ 8_kyu/name-shuffler.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/559ac78160f0be07c200005a
+
+const nameShuffler = str => str.split(' ').reverse().join(' ')
+
+// Output:
+console.log(nameShuffler('john McClane')); //Expected output: 'McClane john'
